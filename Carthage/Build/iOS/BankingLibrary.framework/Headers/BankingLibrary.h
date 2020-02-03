87d08239bbce62abffaf8c464a90e71b7bb70b1f
Halit ŞENARSLAN <halit.senarslan@aktifbank.com.tr>
2020-02-03 15:45:53 +0300

    shared data

---
+++ Carthage/Build/iOS/BankingLibrary.framework/Headers/BankingLibrary.h
@@ -0,0 +1,19 @@
+//
+//  BankingLibrary.h
+//  BankingLibrary
+//
+//  Created by Halit ŞENARSLAN on 3.02.2020.
+//  Copyright © 2020 Halit ŞENARSLAN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BankingLibrary.
+FOUNDATION_EXPORT double BankingLibraryVersionNumber;
+
+//! Project version string for BankingLibrary.
+FOUNDATION_EXPORT const unsigned char BankingLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BankingLibrary/PublicHeader.h>
+
+
